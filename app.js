7b63cf70d262919cbcb4b5507bded62aac040e70
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-07-15 18:58:30 +0800

    feat: Express Setup & add mongoose & add user seeder

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars') 
+const port = 3000
+require('./config/mongoose')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`)
+})
